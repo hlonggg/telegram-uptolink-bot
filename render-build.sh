c908f271fb84a2f1b80ee837733db55ed7ae2e96
hlonggg <139870340+hlonggg@users.noreply.github.com>
2026-06-29 03:32:57 +0700

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Cập nhật và cài Chromium + ChromeDriver
 apt-get update
 apt-get install -y chromium-driver chromium
-
-# Tạo symlink để Selenium tìm thấy
 ln -s /usr/bin/chromium /usr/bin/chromium-browser
-
-echo "[+] Chromium đã được cài đặt"
